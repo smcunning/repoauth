09e0ed2b9547b911c2d431e4d3a6f22646de22be
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2021-01-02 23:12:41 -0700

    Create dashboard controller and dashboard view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,14 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_or_create_by(uid: user_info[:uid])
-    user.uid = request_hash[:uid]
-    user.username = request_hash[:info][:nickname]
-    user.token = request_hash[:credentials][:token]
+    user.uid = user_info[:uid]
+    user.username = user_info[:info][:nickname]
+    user.token = user_info[:credentials][:token]
     user.save
 
     session[:user_id] = user.id
 
-    redirect_to dashboard_path
+    redirect_to dashboard_index_path
   end
 
   private
