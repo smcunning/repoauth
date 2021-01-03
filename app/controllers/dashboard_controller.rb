967d1d8f95ddda3df2b7143b2a9c6a46fee5d596
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2021-01-02 23:35:12 -0700

    Working solution for OAuth and private and public repo lists

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,3 +1,17 @@
 class DashboardController < ApplicationController
-  def index; end
+  def index
+
+    conn = Faraday.new('https://api.github.com', headers: {'Authorization': "token #{current_user.token}"})
+    public_response = conn.get('/user/repos?per_page=100&type=public')
+    all_public_data = JSON.parse(public_response.body, symbolize_names: true)
+    @public = all_public_data.map do |repo|
+      Repo.new(repo)
+    end
+
+    private_response = conn.get('/user/repos?per_page=100&type=private')
+    all_private_data = JSON.parse(private_response.body, symbolize_names: true)
+    @private = all_private_data.map do |repo|
+      Repo.new(repo)
+    end
+  end
 end
